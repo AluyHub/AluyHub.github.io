41865656d5b399d0d6341523b688e221733a2fe1
AluyHub <aluy27@Foxmail.com>
2021-05-29 12:09:25 +0800

    complete carouselMap

--- js/carouselMap.js
+++ js/carouselMap.js
@@ -2,13 +2,31 @@
 	var imgContain = document.getElementsByClassName('imgContent')[0];
 
 	var index = 0;
-	function imgMove() {
-		if(index > 5){
-			index = 0;
-		}
-		index ++;
-		// imgContain.style.left = -index * 500 + 'px'; 
-		console.log(index);
-		setTimeout(imgMove, 1000)
+	var speed = 10;
+	var current = parseInt(getComputedStyle(imgContain,null).left);
+	var nextLeft = 0; 
+	var imgWidth = parseInt(getComputedStyle(imgContain.children[0],null).width);
+	var len = imgContain.children.length;
+	function changeImg() {
+			if(index == len) {
+				index = 1;
+			}
+			setTimeout(changeImg,2000);
+			nextLeft = -index * imgWidth;
+			console.log(nextLeft);
+			animate()
+			index ++;
 	}
-	imgMove();
+	function animate() {
+		if(current > nextLeft) {
+			current -= speed;
+			imgContain.style.left = current + 'px';
+			setTimeout(animate, 10)
+		}else if(current == (imgWidth*(-len+1))){
+			current = 0;
+			imgContain.style.left = '0px';
+		}else{
+			clearTimeout(animate);
+		}
+	}
+	changeImg();
