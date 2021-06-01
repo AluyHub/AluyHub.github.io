18bd2270452be89df567e8919ab12d113fa7c571
AluyHub <aluy27@Foxmail.com>
2021-06-02 01:22:57 +0800

    初试移动端适配

--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
 (function () {
+	var meta = document.getElementsByTagName('meta')[1];
+	meta.content = "width=device-width, initial-scale=" + document.body.offsetWidth / 520 + ", user-scalable=0";
+	console.log(document.body.offsetWidth);
 	InitChangeImg();
 }())
