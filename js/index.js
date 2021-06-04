35e350a9d22a74e76707b2a29c43569da4b31cf4
AluyHub <aluy27@Foxmail.com>
2021-06-04 13:33:09 +0800

    00

--- js/index.js
+++ js/index.js
@@ -1,7 +1,6 @@
 (function () {
 	var meta = document.getElementsByTagName('meta')[1];
-	var scaleSize = document.body.offsetWidth / 520;
 	meta.content = "width=device-width, initial-scale=" + 
-	scaleSize + ", maximum-scale=" + scaleSize + ", user-scalable=no";
+	document.body.offsetWidth / 520 + ", maximum-scale=" + document.body.offsetWidth / 520 + ", user-scalable=no";
 	InitChangeImg();
 }())
